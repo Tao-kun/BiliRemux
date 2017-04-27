29c4d129ca443780a70b36e1f1e76277d7cd1097
Tao-kun <zhuoytxj@163.com>
2017-04-27 21:32:33 +0800

    Change into PEP-8

--- remuxer.py
+++ remuxer.py
@@ -2,8 +2,8 @@
 import glob
 import json
 import os
-import sys
 import shutil
+import sys
 
 from config import *
 
@@ -15,14 +15,14 @@ def remux(flv_video_path):
         file_name = file.split(os.sep)[-1]
         if len(file_name) < 10 and len(file_name) >= 5:
             video_list.append(os.path.join(flv_video_path, file_name))
-    if len(video_list)==1:
-        #MP4
-        if video_list[0].split('.')[-1]=='mp4':
+    if len(video_list) == 1:
+        # MP4
+        if video_list[0].split('.')[-1] == 'mp4':
             shutil.move(video_list[0], '{}_remux.mp4'.format(flv_video_path))
-        #blv/flv
+        # blv/flv
         else:
             command = FFMPEG_PATH + ' -i {}  -y -vcodec copy -acodec copy {}_remux.mp4'
-            os.system(command.format(video_list[0],flv_video_path))
+            os.system(command.format(video_list[0], flv_video_path))
     else:
         command = FFMPEG_PATH + ' -i "concat:{}" -c copy -bsf:a aac_adtstoasc {}_remux.mp4'
         os.system(command.format('|'.join(video_list), flv_video_path))
@@ -53,7 +53,7 @@ def find_flv_path(video_part_path):
         return None
     video_file_name = video_information['type_tag']
     if not glob.glob('{}{}{}_remux.mp4'.format(video_part_path, os.sep, video_file_name)):
-        print(video_part_path,video_file_name)
+        print(video_part_path, video_file_name)
         flv_video_path = glob.glob('{}{}{}'.format(video_part_path, os.sep, video_file_name))[0]
         return flv_video_path
     else:
